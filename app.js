fff69878fab4c9e21bd7914c97ebe02981d9a732
Alain MUCYO <alainmucyo3@gmail.com>
2021-07-17 08:19:44 +0200

    Change response

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(express.json())
 app.get("/gallery/:galleryId", (async (req, res) => {
     const {galleryId} = req.params
     const {data} = await axios.get(`https://jsonplaceholder.typicode.com/albums/${galleryId}/photos`)
-    res.send({message: "Hello", data})
+    res.send(data)
 }))
 
 app.listen(process.env.PORT || 5000, () => {
